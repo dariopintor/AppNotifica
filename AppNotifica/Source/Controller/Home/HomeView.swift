bac935a0a3f0edccaf576b64264a9ea936cfb9a1
José Dario Pintor da Silva <dariocomp@gmail.com>
2023-12-04 14:03:21 -0300

    Criando a tableView na tela Home
    Criando a tableView na tela Home

--- AppNotifica/Source/Controller/Home/HomeView.swift
+++ AppNotifica/Source/Controller/Home/HomeView.swift
@@ -3,8 +3,6 @@
 //  AppNotifica
 //
 //  Created by Dario Pintor on 21/10/22.
-//
-
 import Foundation
 import UIKit
 class HomeView: ViewDefault {
@@ -21,13 +19,47 @@ class HomeView: ViewDefault {
         super.init(frame: .zero)
     }
     
+    lazy var tableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .grouped)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    } ()
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    override  func setupVisualElements() {
+       super.setupVisualElements()
+        
+       addSubview(tableView)
+       NSLayoutConstraint.activate([
+        tableView.topAnchor.constraint(equalTo: topAnchor),
+        tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+        tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+       ])
+        
+       setupTableView()
+   }
     
-    
-   override  func setupVisualElements() {
-
+    func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellIdentifier")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+}
+
+extension HomeView: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView:UITableView, cellForRowAt indexPath: IndexPath ) ->
+    UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+        cell.textLabel?.text = "Ocorrência 1"
+        return cell
+    }
 }
